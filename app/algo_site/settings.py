708026f3706883ea80f2b107e3f29b3b8e6dfb4a
Dominik Mühlbacher <dominik.muehlbacher@plusserver.com>
2024-05-13 21:14:25 +0200

    finished https

--- app/algo_site/settings.py
+++ app/algo_site/settings.py
@@ -132,3 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 #CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+CSRF_TRUSTED_ORIGINS = "https://dominikmuehlbacher.at"
